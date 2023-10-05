a369a10f06504837c1d328a4e25b048d34274d87
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-10-05 11:59:13 +0200

    feat: or-1666 get uitnodiging by Id en inszÃ

---
+++ src/AssociationRegistry.Invitations.Api/Uitnodigingen/Controllers/GetUitnodigingVoorPersoon.cs
@@ -0,0 +1,40 @@
+﻿using AssociationRegistry.Invitations.Api.Uitnodigingen.Mapping;
+using AssociationRegistry.Invitations.Api.Uitnodigingen.Queries;
+using AssociationRegistry.Invitations.Api.Uitnodigingen.Responses;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Uitnodiging = AssociationRegistry.Invitations.Api.Uitnodigingen.Models.Uitnodiging;
+
+namespace AssociationRegistry.Invitations.Api.Uitnodigingen.Controllers;
+
+public class GetUitnodigingVoorPersoon : ControllerBase
+{
+    private readonly IQuerySession _session;
+
+    public GetUitnodigingVoorPersoon(IQuerySession session)
+    {
+        _session = session;
+    }
+
+    [HttpGet("/personen/{insz}/uitnodigingen/{uitnodigingsId}")]
+    public async Task<IActionResult> Get([FromRoute] string insz, [FromRoute] Guid uitnodigingsId,
+        CancellationToken cancellationToken)
+    {
+        var uitnodiging = await _session
+            .LoadAsync<Uitnodiging>(uitnodigingsId, cancellationToken);
+        if (uitnodiging is null)
+        {
+            ModelState.AddModelError("UitnodigingsId", "Deze uitnodiging is niet gekend.");
+            return ValidationProblem(ModelState);
+        }
+
+        if (uitnodiging.Uitgenodigde.Insz != insz)
+        {
+            ModelState.AddModelError("Insz", "Deze uitnodiging is niet voor deze persoon bestemd.");
+            return ValidationProblem(ModelState);
+        }
+
+        return Ok(uitnodiging.ToDetail());
+    }
+}
---
+++ test/AssociationRegistry.Invitations.Api.Tests/BijHetOpvragenvanEenUitnodiging/GegevenEenWeigering.cs
@@ -0,0 +1,91 @@
+﻿using System.Globalization;
+using System.Net;
+using AssociationRegistry.Invitations.Api.Tests.Autofixture;
+using AssociationRegistry.Invitations.Api.Tests.Fixture;
+using AssociationRegistry.Invitations.Api.Uitnodigingen.Models;
+using AssociationRegistry.Invitations.Api.Uitnodigingen.Requests;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NodaTime;
+
+namespace AssociationRegistry.Invitations.Api.Tests.BijHetOpvragenvanEenUitnodiging;
+
+[Collection(UitnodigingenApiCollection.Name)]
+public class GegevenEenWeigering : IClassFixture<GegevenEenWeigering.Setup>
+{
+    private readonly UitnodigingenApiClient _client;
+    private readonly Setup _setup;
+
+    public GegevenEenWeigering(UitnodigingenApiFixture fixture, Setup setup)
+    {
+        _setup = setup;
+        _client = fixture.Clients.Authenticated;
+    }
+
+    [Fact]
+    public async Task DanIsDeResponse200()
+    {
+        var response = await _client.GetUitnodigingsDetail(_setup.Uitnodiging.Uitgenodigde.Insz, _setup.UitnodigingsId);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task DanBevatDeBodyDeGeregistreerdeUitnodiging()
+    {
+        var response = await _client.GetUitnodigingsDetail(_setup.Uitnodiging.Uitgenodigde.Insz, _setup.UitnodigingsId);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var uitnodiging = JsonConvert.DeserializeObject<JObject>(content,
+            new JsonSerializerSettings { DateParseHandling = DateParseHandling.None });
+        uitnodiging["id"].Value<string>().Should().Be(_setup.UitnodigingsId.ToString());
+        uitnodiging["vCode"].Value<string>().Should().Be(_setup.Uitnodiging.VCode);
+        uitnodiging["boodschap"].Value<string>().Should().Be(_setup.Uitnodiging.Boodschap);
+        uitnodiging["status"].Value<string>().Should().Be(UitnodigingsStatus.Geweigerd.Status);
+        uitnodiging["datumLaatsteAanpassing"].Value<string>().Should()
+            .Be(_setup.UitnodigingAanvaardOp.ToString("g", CultureInfo.InvariantCulture));
+        uitnodiging["uitnodiger"]["vertegenwoordigerId"].Value<int>().Should()
+            .Be(_setup.Uitnodiging.Uitnodiger.VertegenwoordigerId);
+        uitnodiging["uitgenodigde"]["insz"].Value<string>().Should().Be(_setup.Uitnodiging.Uitgenodigde.Insz);
+        uitnodiging["uitgenodigde"]["naam"].Value<string>().Should().Be(_setup.Uitnodiging.Uitgenodigde.Naam);
+        uitnodiging["uitgenodigde"]["voornaam"].Value<string>().Should().Be(_setup.Uitnodiging.Uitgenodigde.Voornaam);
+        uitnodiging["uitgenodigde"]["email"].Value<string>().Should().Be(_setup.Uitnodiging.Uitgenodigde.Email);
+    }
+
+    public class Setup : IDisposable, IAsyncLifetime
+    {
+        public UitnodigingsRequest Uitnodiging { get; set; }
+        public Guid UitnodigingsId { get; set; }
+        public Instant UitnodigingAanvaardOp { get; set; }
+
+        private readonly UitnodigingenApiClient _client;
+        private UitnodigingenApiFixture _fixture;
+
+        public Setup(UitnodigingenApiFixture fixture)
+        {
+            _fixture = fixture;
+            _client = fixture.Clients.Authenticated;
+
+            Uitnodiging = new AutoFixture.Fixture()
+                .Customize(new GeldigeUitnodigingen())
+                .Create<UitnodigingsRequest>();
+        }
+
+        public void Dispose()
+        {
+            _fixture.ResetDatabase();
+        }
+
+        public async Task InitializeAsync()
+        {
+            var response = await _client.RegistreerUitnodiging(Uitnodiging);
+            var content = await response.Content.ReadAsStringAsync();
+            UitnodigingsId = Guid.Parse(JToken.Parse(content)["id"]!.Value<string>()!);
+
+            await _client.WeigerUitnodiging(UitnodigingsId);
+
+            UitnodigingAanvaardOp = _fixture.Clock.PreviousInstant;
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+    }
+}
