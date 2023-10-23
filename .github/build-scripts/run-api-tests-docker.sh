f972e0c4d7428e7e1edde20435a85357c2497291
Jan Lesage <jan.lesage@vlaanderen.be>
2023-10-23 13:16:12 +0200

    fix: OR-1804 added archiver tests in pre-merge script

--- .github/build-scripts/run-api-tests-docker.sh
+++ .github/build-scripts/run-api-tests-docker.sh
@@ -2,7 +2,5 @@ dotnet tool restore;
 dotnet paket restore -v;
 
 dotnet restore test/AssociationRegistry.Invitations.Api.Tests --runtime linux-x64
-
 dotnet build --no-restore --runtime 'linux-x64' test/AssociationRegistry.Invitations.Api.Tests
-
 dotnet test test/AssociationRegistry.Invitations.Api.Tests --logger html
