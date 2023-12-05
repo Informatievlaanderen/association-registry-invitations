2600f287714cf1559ec531de677017dedb80dc06
Koen Metsu <koen@kometsoft.be>
2023-12-05 12:46:41 +0100

    fix: link solution info so version is displayed in docs

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.199")]
 [assembly: AssemblyFileVersion("1.0.199")]
-[assembly: AssemblyInformationalVersion("unknown")]
-[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
