2e5d7bb89fe07211c1ceac0fc9dc60fbf6a42ee4
emalfroy <egon.malfroy@gmail.com>
2024-06-04 11:02:20 +0200

    feat: or-1566 move to dotnet 8

--- .github/build-scripts/publish.sh
+++ .github/build-scripts/publish.sh
@@ -3,6 +3,6 @@ dotnet paket restore;
 
 dotnet restore src/$1 --runtime linux-x64
 
-dotnet build --no-restore --runtime 'linux-x64' --self-contained -f net6.0 src/$1
-dotnet publish -o dist/$1/linux --no-build --no-restore --runtime 'linux-x64' --self-contained -f net6.0 src/$1
+dotnet build --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
+dotnet publish -o dist/$1/linux --no-build --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
 
