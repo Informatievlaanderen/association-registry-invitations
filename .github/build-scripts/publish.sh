5d3daeeee97f8e9b33147403b9895cf51c8e9157
emalfroy <egon.malfroy@gmail.com>
2024-06-04 13:25:53 +0200

    fix: or-1566 fix build and release in github workflow

--- .github/build-scripts/publish.sh
+++ .github/build-scripts/publish.sh
@@ -3,6 +3,10 @@ dotnet paket restore;
 
 dotnet restore src/$1 --runtime linux-x64
 
-dotnet build --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
-dotnet publish -o dist/$1/linux --no-build --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
+dotnet build -c Release --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
+pushd src/$1
+dotnet publish -c Release -o ../../dist/$1/linux --no-build --no-restore --runtime 'linux-x64' --self-contained -f net8.0
+popd
+
+#dotnet publish -c Release -o dist/$1/linux --no-build --no-restore --runtime 'linux-x64' --self-contained -f net8.0 src/$1
 
